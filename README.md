d4b05e3c4ff0283fdbaa0e569fb4d1598681ebbe
Jonathan Rothwell <j@joro.dev>
2023-04-14 21:29:23 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VersionedCodable
+
+A description of this package.
---
+++ Sources/VersionedCodable/VersionedCodable.swift
@@ -0,0 +1,6 @@
+public struct VersionedCodable {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
