50a0ea29709cc4e14b6b9d07c35cd75679190717
Jonathan Rothwell <j@joro.dev>
2023-06-15 02:12:14 +0100

    WIP: add support for macros

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
-// swift-tools-version: 5.7.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "VersionedCodable",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,10 +15,25 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .macro(
+            name: "VersionedCodableMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+        .testTarget(name: "VersionedCodableMacroTests",
+                    dependencies: [
+                        "VersionedCodableMacros",
+                        .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+                    ]),
+        
         .target(
             name: "VersionedCodable",
             dependencies: [],
---
+++ Tests/VersionedCodableMacroTests/VersionedCodableMacroExpansionTests.swift
@@ -0,0 +1,28 @@
+//
+//  VersionedCodableMacroExpansionTests.swift
+//  
+//
+//  Created by Jonathan Rothwell on 15/06/2023.
+//
+
+import XCTest
+import SwiftSyntaxMacrosTestSupport
+
+final class VersionedCodableMacroExpansionTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
