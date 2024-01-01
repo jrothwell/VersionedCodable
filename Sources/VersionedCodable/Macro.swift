8aa9eac3536c46c81b809298d9ac2b3c80f12883
Jonathan Rothwell <j@joro.dev>
2024-01-01 20:26:14 +0000

    Add documentation for the macro (#5)

--- Sources/VersionedCodable/Macro.swift
+++ Sources/VersionedCodable/Macro.swift
@@ -1,5 +1,5 @@
 //
-//  VersionedCodableMacro.swift
+//  Macro.swift
 //
 //
 //  Created by Jonathan Rothwell on 15/06/2023.
@@ -7,6 +7,15 @@
 
 import Foundation
 
+
+/// Makes the attached type conform to ``VersionedCodable``.
+/// - Parameters:
+///     - v:  The current version of this type. This is what is encoded into the `version` key on this type
+/// when it is encoded.
+///     - previously: The next oldest version of this type, or ``NothingEarlier`` if this *is* the oldest version.
+/// - Note: If your type has a previous version, you will need to implement an initializer for it. This is where you
+///   do the mapping between the old and the new version of the type.
+/// - SeeAlso: ``VersionedCodable``
 @attached(extension, conformances: VersionedCodable, names: named(version), named(PreviousVersion))
 public macro versionedCodable(v: Int?, previously: any VersionedCodable.Type) = #externalMacro(
     module: "VersionedCodableMacros",
