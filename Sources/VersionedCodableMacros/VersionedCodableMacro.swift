0c1b00091645aae492fa8f99e789b2817bc6527d
Jonathan Rothwell <j@joro.dev>
2024-01-02 00:43:37 +0000

    Add more tests & reduce code duplication (#5)

--- Sources/VersionedCodableMacros/VersionedCodableMacro.swift
+++ Sources/VersionedCodableMacros/VersionedCodableMacro.swift
@@ -7,6 +7,30 @@
 import SwiftSyntax
 import SwiftSyntaxMacros
 import SwiftSyntaxBuilder
+import SwiftDiagnostics
+
+public enum VersionedCodableMacroError: CustomStringConvertible, Error {
+    
+    /// The current version has not been specified.
+    /// - Note: In principle this will not happen, because the arguments for the exported macro are strongly
+    ///   typed. This, however, allows us to test the compiler plugin.
+    case missingCurrentVersion
+    
+    /// The previous version has not been specified.
+    /// - Note: In principle this will not happen, because the arguments for the exported macro are strongly
+    ///   typed. This, however, allows us to test the compiler plugin.
+    case missingPreviousVersion
+    
+    public var description: String {
+        switch self {
+        case .missingCurrentVersion:
+            return "You must specify the current version of the type, even if there isn't one."
+        case .missingPreviousVersion:
+            return "You must specify a previous version of the type, even if there isn't one."
+        }
+    }
+    
+}
 
 public struct VersionedCodableMacro {}
 
@@ -14,9 +38,24 @@ extension VersionedCodableMacro: ExtensionMacro {
     public static func expansion(of node: SwiftSyntax.AttributeSyntax,
                                  attachedTo declaration: some SwiftSyntax.DeclGroupSyntax,
                                  providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol,
-                                 conformingTo protocols: [SwiftSyntax.TypeSyntax], in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
-        guard let version = node.version,
-              let previousVersion = node.previousVersion else { return [] }
+                                 conformingTo protocols: [SwiftSyntax.TypeSyntax],
+                                 in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
+        
+        guard case .argumentList(let arguments) = node.arguments else {
+            context.addDiagnostics(from: VersionedCodableMacroError.missingCurrentVersion, node: node)
+            context.addDiagnostics(from: VersionedCodableMacroError.missingPreviousVersion, node: node)
+            return []
+        }
+        
+        guard let version = arguments.version else {
+            context.addDiagnostics(from: VersionedCodableMacroError.missingCurrentVersion, node: node)
+            return []
+        }
+        
+        guard let previousVersion = arguments.previousVersion else {
+            context.addDiagnostics(from: VersionedCodableMacroError.missingPreviousVersion, node: node)
+            return []
+        }
 
         return [
             DeclSyntax("""
@@ -27,35 +66,23 @@ extension VersionedCodableMacro: ExtensionMacro {
                        """).cast(ExtensionDeclSyntax.self)
         ]
     }
-    
-    
 }
 
-private extension SwiftSyntax.AttributeSyntax {
+private extension LabeledExprListSyntax {
     var version: TokenSyntax? {
-        guard case .argumentList(let arguments) = self.arguments else {
-            return nil
-        }
-        
-        guard let expression = arguments.filter({
-            guard case .identifier(let label) = $0.label?.tokenKind else { return false }
+        self.filter({
+            guard case .identifier(let label) =
+                    $0.label?.tokenKind else { return false }
             return label == "v"
-        }).first?.expression else { return nil }
-        
-        return expression.firstToken(viewMode: .fixedUp)!
+        }).first?.expression.firstToken(viewMode: .fixedUp)
     }
     
     var previousVersion: TokenSyntax? {
-        guard case .argumentList(let arguments) = self.arguments else {
-            return nil
-        }
-        
-        guard let expression = arguments.filter({
+        self.filter({
             guard case .identifier(let label) = $0.label?.tokenKind else { return false }
             return label == "previously"
-        }).first?.expression else { return nil }
-        
-        return expression.firstToken(viewMode: .fixedUp)!
+        }).first?.expression.firstToken(viewMode: .fixedUp)
     }
+
 }
 
--- Tests/VersionedCodableMacroTests/VersionedCodableExtensionMacroExpansionTests.swift
+++ Tests/VersionedCodableMacroTests/VersionedCodableExtensionMacroExpansionTests.swift
@@ -106,5 +106,86 @@ final class VersionedCodableExtensionMacroExpansionTests: XCTestCase {
             }
             """, macros: ["versionedCodable": VersionedCodableMacro.self])
     }
+    
+    func testExpansionWhereMissingPreviousType() throws {
+        assertMacroExpansion(
+            """
+            @versionedCodable(v: nil)
+            struct Poem {
+                var author: String
+                var body: String
+            }
+            """,
+            expandedSource:
+            """
+            
+            struct Poem {
+                var author: String
+                var body: String
+            }
+            """,
+            diagnostics: [
+                DiagnosticSpec(
+                    message: "You must specify a previous version of the type, even if there isn't one.",
+                    line: 1,
+                    column: 1),
+            ],
+            macros: ["versionedCodable": VersionedCodableMacro.self])
+    }
+    
+    func testExpansionWhereMissingVersion() throws {
+        assertMacroExpansion(
+            """
+            @versionedCodable(previously: OldPoem.self)
+            struct Poem {
+                var author: String
+                var body: String
+            }
+            """,
+            expandedSource:
+            """
+            
+            struct Poem {
+                var author: String
+                var body: String
+            }
+            """,
+            diagnostics: [
+                DiagnosticSpec(
+                    message: "You must specify the current version of the type, even if there isn't one.",
+                    line: 1,
+                    column: 1),
+            ],
+            macros: ["versionedCodable": VersionedCodableMacro.self])
+    }
 
+    func testExpansionWhereMissingBothArguments() throws {
+        assertMacroExpansion(
+            """
+            @versionedCodable
+            struct Poem {
+                var author: String
+                var body: String
+            }
+            """,
+            expandedSource:
+            """
+            
+            struct Poem {
+                var author: String
+                var body: String
+            }
+            """,
+            diagnostics: [
+                DiagnosticSpec(
+                    message: "You must specify the current version of the type, even if there isn't one.",
+                    line: 1,
+                    column: 1),
+                DiagnosticSpec(
+                    message: "You must specify a previous version of the type, even if there isn't one.",
+                    line: 1,
+                    column: 1)
+            ],
+            macros: ["versionedCodable": VersionedCodableMacro.self])
+    }
 }
