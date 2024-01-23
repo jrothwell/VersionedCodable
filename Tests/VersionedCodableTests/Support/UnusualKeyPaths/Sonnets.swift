30cfc2586f57c94b1d089a76ac9672154a8b614e
Jonathan Rothwell <j@joro.dev>
2024-01-23 01:59:54 +0000

    Complete tests & test supporting types (#1)

--- Tests/VersionedCodableTests/Support/UnusualKeyPaths/Sonnets.swift
+++ Tests/VersionedCodableTests/Support/UnusualKeyPaths/Sonnets.swift
@@ -8,7 +8,48 @@
 import Foundation
 import VersionedCodable
 
-struct Sonnet: VersionedCodable {
+struct SonnetV2: VersionedCodable {
+    static let version: Int? = 2
+    typealias PreviousVersion = SonnetV1
+    
+    var author: String
+    var body: [BodyElement]
+    
+    enum BodyElement: Codable {
+        case quatrain([String])
+        case couplet([String])
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            let rawBody = try container.decode(RawBodyElement.self)
+            switch rawBody.kind {
+            case .quatrain:
+                self = .quatrain(rawBody.body)
+            case .couplet:
+                self = .couplet(rawBody.body)
+            }
+        }
+        
+        struct RawBodyElement: Codable {
+            var kind: Kind
+            var body: [String]
+            
+            enum Kind: String, Codable {
+                case quatrain
+                case couplet
+            }
+        }
+    }
+    
+    init(from old: SonnetV1) throws {
+        self.author = old.author
+        self.body = old.body.quatrains.map { BodyElement.quatrain($0) } +
+        old.body.couplets.map { BodyElement.couplet($0) }
+    }
+
+}
+
+struct SonnetV1: VersionedCodable {
     static let version: Int? = 1
     typealias PreviousVersion = NothingEarlier
     
--- Tests/VersionedCodableTests/UnusualVersionKeyPathsTests.swift
+++ Tests/VersionedCodableTests/UnusualVersionKeyPathsTests.swift
@@ -16,15 +16,8 @@ final class UnusualVersionKeyPathsTests: XCTestCase {
         let data = try Data(
             contentsOf: Bundle.module.url(forResource: "sonnet-v1",
                                           withExtension: "json")!)
-        let decoded = try JSONDecoder().decode(versioned: Sonnet.self, from: data)
+        let decoded = try JSONDecoder().decode(versioned: SonnetV1.self, from: data)
         XCTAssertEqual("William Shakespeare", decoded.author)
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
