48ad6e4bb5ec58d2200611115584dd2396dccd05
Jonathan Rothwell <j@joro.dev>
2024-06-11 01:08:39 +0100

    experiment: Start moving to swift-testing

--- Tests/VersionedCodableTests/NothingEarlierConformanceConfidenceTests.swift
+++ Tests/VersionedCodableTests/NothingEarlierConformanceConfidenceTests.swift
@@ -6,37 +6,55 @@
 //
 
 import XCTest
-import VersionedCodable
+import Testing
+@testable import VersionedCodable
 
-final class NothingEarlierConformanceConfidenceTests: XCTestCase {
+@Suite("NothingEarlier")
+struct NothingEarlierConformanceConfidenceTests {
     
     let blankData = "{}".data(using: .utf8)!
     
-    func testNothingEarlierVersionIsNil() throws {
-        XCTAssertNil(NothingEarlier.version)
+    @Test(
+        "has a version of `nil`",
+        .tags(.configuration)
+    ) func nothingEarlierVersionIsNil() throws {
+        #expect(NothingEarlier.version == nil)
     }
     
-    func testDecodingThrowsError() throws {
-        XCTAssertThrowsError(try JSONDecoder().decode(NothingEarlier.self, from: blankData)) { error in
+    @Test(
+        "throws if you try to decode anything into it",
+        .tags(.configuration)
+    ) func decodingNothingEarlierThrowsAnError() throws {
+        
+        // TODO: 20/09/2024: The helper function is necessary due to some kind of issue with macro expansion. Remove this once the bug (in the compiler?) is resolved.
+        func isTypeMismatchVsNothingEarlier(error: Error) -> Bool {
             switch error {
             case DecodingError.typeMismatch(let type, _):
-                XCTAssertTrue(type == NothingEarlier.self)
+                return type == NothingEarlier.self
             default:
-                XCTFail("An error threw, but it was the wrong kind of error (expected `VersionedDecodingError.unsupportedVersion(tried:)`, got: \(error)")
+                return false
             }
-
         }
         
+        #expect {
+            try JSONDecoder().decode(
+                NothingEarlier.self,
+                from: blankData
+            )
+        } throws: { error in
+            return isTypeMismatchVsNothingEarlier(error: error)
+        }
     }
     
-    func testDecodingFromSlightlyEarlierType() throws {
-        XCTAssertThrowsError(try JSONDecoder().decode(versioned: VersionedCodableWithoutOlderVersion.self, from: blankData)) { error in
-            switch error {
-            case VersionedDecodingError.unsupportedVersion(let currentVersion):
-                XCTAssertTrue(currentVersion == VersionedCodableWithoutOlderVersion.self)
-            default:
-                XCTFail("An error threw, but it was the wrong kind of error (expected `VersionedDecodingError.unsupportedVersion(tried:)`, got: \(error)")
-            }
+    @Test(
+        "works properly as the 'stopper' type where there are no previous versions",
+        .tags(.behaviour)
+    ) func decodingFromSlightlyEarlierType() throws {
+        #expect(throws: VersionedDecodingError.unsupportedVersion(tried: VersionedCodableWithoutOlderVersion.self)) {
+            try JSONDecoder().decode(
+                versioned: VersionedCodableWithoutOlderVersion.self,
+                from: blankData
+            )
         }
     }
 }
@@ -47,4 +65,19 @@ struct VersionedCodableWithoutOlderVersion: VersionedCodable {
     typealias PreviousVersion = NothingEarlier
     
     var text: String
+}
+
+extension VersionedDecodingError: @retroactive Equatable {
+    public static func == (lhs: VersionedDecodingError, rhs: VersionedDecodingError) -> Bool {
+        switch (lhs, rhs) {
+        case let (.unsupportedVersion(leftVersion), .unsupportedVersion(rightVersion)):
+            leftVersion == rightVersion
+        case (.fieldNoLongerValid, .fieldNoLongerValid):
+            true
+        default:
+            false
+        }
+    }
+    
+    
 }
