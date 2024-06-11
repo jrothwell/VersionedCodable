768cf36249999bd5808e3130c4937c0db19ea068
Jonathan Rothwell <j@joro.dev>
2024-06-12 00:48:07 +0100

    Update next tests

--- Tests/VersionedCodableTests/VersionedCodableJSONTests.swift
+++ Tests/VersionedCodableTests/VersionedCodableJSONTests.swift
@@ -1,4 +1,5 @@
-import XCTest
+import Testing
+import Foundation
 @testable import VersionedCodable
 
 struct VersionedDocument: Codable {
@@ -6,9 +7,10 @@ struct VersionedDocument: Codable {
 }
 
 
-final class VersionedCodableJSONTests: XCTestCase {
-
-    func testDecodingPoemPreV1ToPoemV1() throws {
+@Suite("JSONDecoder encoding/decoding", .tags(.behaviour))
+struct VersionedCodableJSONTests {
+    @Test("decodes a grandfathered type with no version field")
+    func decodingPoemPreV1ToPoemV1() throws {
         let oldPoem = """
                       {
                         "author": "John Smith",
@@ -17,14 +19,15 @@ final class VersionedCodableJSONTests: XCTestCase {
                       """.data(using: .utf8)!
         
         
-        let migrated = try JSONDecoder().decode(
-            versioned: Poem.PoemV1.self,
-            from: oldPoem)
-        XCTAssertEqual(migrated.author, "John Smith")
-        XCTAssertEqual(migrated.poem, "Hello")
+        let migrated = try JSONDecoder()
+            .decode(versioned: Poem.PoemV1.self,
+                    from: oldPoem)
+        #expect("John Smith" == migrated.author)
+        #expect("Hello" == migrated.poem)
     }
     
-    func testDecodingPoemPreV1ToPoemV1WhereWeDiscardDueToNewMandatoryFields() throws {
+    @Test("throws when trying to decode type if some fields are now mandatory")
+    func decodingPoemPreV1ToPoemV1ThrowsDueToNewMandatoryFields() throws {
         let oldPoem = """
                       {
                         "author": "John Smith",
@@ -32,20 +35,29 @@ final class VersionedCodableJSONTests: XCTestCase {
                       }
                       """.data(using: .utf8)!
         
-        XCTAssertThrowsError(try JSONDecoder().decode(
-            versioned: Poem.PoemV1.self,
-            from: oldPoem)) { error in
-                switch error {
-                case VersionedDecodingError.fieldNoLongerValid(let context):
-                    XCTAssertEqual("poem", context.codingPath[0].stringValue)
-                    XCTAssertEqual("Poem is no longer optional", context.debugDescription)
-                default:
-                    XCTFail("An error threw, but it was the wrong kind of error (expected `VersionedDecodingError.fieldNoLongerValid`, got: \(error))")
-                }
-            }
+        
+        #expect {
+            try JSONDecoder().decode(
+                versioned: Poem.PoemV1.self,
+                from: oldPoem
+            )
+        } throws: { error in
+            guard let error = error as? VersionedDecodingError,
+                  case .fieldNoLongerValid(let context) = error else {
+                      return false
+                  }
+            
+            return (
+                "poem" == context.codingPath[0].stringValue
+            ) && (
+                "Poem is no longer optional" == context.debugDescription
+            )
+            
+        }
     }
     
-    func testDecodingOlderVersionThanWeSupportExplodes() throws {
+    @Test("throws when decoding an older version than we support")
+    func throwsWhenDecodingOlderVersionThanWeSupport() throws {
         let oldPoem = """
                       {
                         "version": -1,
@@ -54,19 +66,16 @@ final class VersionedCodableJSONTests: XCTestCase {
                       }
                       """.data(using: .utf8)!
         
-        XCTAssertThrowsError(try JSONDecoder().decode(
-            versioned: Poem.PoemV1.self,
-            from: oldPoem)) { error in
-                switch error {
-                case VersionedDecodingError.unsupportedVersion(let olderVersion):
-                    XCTAssertTrue(olderVersion == Poem.PoemPreV1.self)
-                default:
-                    XCTFail("An error threw, but it was the wrong kind of error (expected `VersionedDecodingError.unsupportedVersion(Poem.PoemPreV1)`, got: \(error))")
-                }
-            }
+        #expect(throws: VersionedDecodingError.unsupportedVersion(tried: Poem.PoemPreV1.self)) {
+            try JSONDecoder().decode(
+                versioned: Poem.PoemV1.self,
+                from: oldPoem
+            )
+        }
     }
     
-    func testMultiStageMigration() throws {
+    @Test("performs a multi-stage migration")
+    func multiStageMigration() throws {
         let oldPoem = """
                       {
                         "version": 1,
@@ -75,13 +84,23 @@ final class VersionedCodableJSONTests: XCTestCase {
                       }
                       """.data(using: .utf8)!
         
-        let migrated = try JSONDecoder()
-            .decode(versioned: Poem.self,
-                    from: oldPoem)
-        XCTAssertEqual(["An epicure dining at Crewe", "Found a rather large mouse in his stew", "Cried the waiter: Don\'t shout", "And wave it about", "Or the rest will be wanting one too!"], migrated.lines)
-        XCTAssertEqual("A Clever Man", migrated.author?.name)
+        let migrated = try JSONDecoder().decode(
+            versioned: Poem.self,
+            from: oldPoem)
+        
+        #expect(
+            [
+                "An epicure dining at Crewe",
+                "Found a rather large mouse in his stew",
+                "Cried the waiter: Don\'t shout",
+                "And wave it about",
+                "Or the rest will be wanting one too!"
+            ] == migrated.lines
+        )
+        #expect("A Clever Man" == migrated.author?.name)
     }
     
+    @Test("encodes the version properly")
     func testVersionEncodedCorrectly() throws {
         let oldPoem = """
                       {
@@ -94,16 +113,25 @@ final class VersionedCodableJSONTests: XCTestCase {
         let migrated = try JSONDecoder()
             .decode(versioned: Poem.self,
                     from: oldPoem)
+        
 
         let encoded = try JSONEncoder().encode(versioned: migrated)
         
         let versionedDocument = try JSONDecoder().decode(VersionedDocument.self, from: encoded)
         
-        XCTAssertEqual(4, versionedDocument.version)
+        #expect(4 == versionedDocument.version)
         
         let encodedAndDecodedDocument = try JSONDecoder().decode(Poem.self, from: encoded)
-        XCTAssertEqual(["An epicure dining at Crewe", "Found a rather large mouse in his stew", "Cried the waiter: Don\'t shout", "And wave it about", "Or the rest will be wanting one too!"], encodedAndDecodedDocument.lines)
-        XCTAssertEqual("A Clever Man", encodedAndDecodedDocument.author?.name)
+        #expect(
+            [
+                "An epicure dining at Crewe",
+                "Found a rather large mouse in his stew",
+                "Cried the waiter: Don\'t shout",
+                "And wave it about",
+                "Or the rest will be wanting one too!"
+            ] == encodedAndDecodedDocument.lines
+        )
+        #expect("A Clever Man" == encodedAndDecodedDocument.author?.name)
     }
 
 }
