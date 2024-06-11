11c46ed88915c8698562bb1e03f6256a17d38eb2
Jonathan Rothwell <j@joro.dev>
2024-06-11 23:25:35 +0100

    Remove nonisolated(unsafe) from protocol & add documentation.

--- Sources/VersionedCodable/VersionPathSpec.swift
+++ Sources/VersionedCodable/VersionPathSpec.swift
@@ -10,8 +10,12 @@ import Foundation
 /// Describes how to decode or encode the version of a ``VersionedCodable`` type.
 public protocol VersionPathSpec: Codable {
     /// The key path to the version of this type. Used to find the version key during decoding.
+    ///
+    /// - Warning: It is your responsibility to make sure this value does not change during decoding.
+    ///   Typically you achieve this by making the version key immutable (i.e. a `let` as opposed to a
+    ///   `var`.)
     // TODO: 11/06/2024 - Write a test to attempt to make this break concurrency in some way & then work around it.
-    nonisolated(unsafe) static var keyPathToVersion: KeyPath<Self, Int?> { get }
+    static var keyPathToVersion: KeyPath<Self, Int?> { get }
     
     /// Initializes the type with the provided version.
     /// - Parameter version: The version of the document being encoded.
@@ -21,12 +25,12 @@ public protocol VersionPathSpec: Codable {
 
 /// Describes how to encode and decode the version of a ``VersionedCodable`` type where the version is encoded at the root of the type in a field called `version`.
 public struct VersionKeyAtRootVersionPathSpec: Codable, VersionPathSpec {
-    nonisolated(unsafe) public static let keyPathToVersion: KeyPath<VersionKeyAtRootVersionPathSpec, Int?> = \Self.version
+    nonisolated(unsafe) public static let keyPathToVersion: KeyPath<Self, Int?> = \Self.version
     public init(withVersion version: Int? = nil) {
         self.version = version
     }
     
-    var version: Int?
+    let version: Int?
 }
 
 
