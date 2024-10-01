f58ea362fd452f51dec9a324d2890290697b9508
Jonathan Rothwell <j@joro.dev>
2024-10-01 08:35:47 +0100

    Remove comments from package

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VersionedCodable",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "VersionedCodable",
             targets: ["VersionedCodable"]),
@@ -15,8 +13,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VersionedCodable",
             dependencies: [],
