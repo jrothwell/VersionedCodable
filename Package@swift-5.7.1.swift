158c077a11453f6e59d439736ddf1f1aad96eef5
Jonathan Rothwell <j@joro.dev>
2024-10-03 07:07:16 +0100

    Remove DocC from legacy Package.swift

--- Package@swift-5.7.1.swift
+++ Package@swift-5.7.1.swift
@@ -9,9 +9,6 @@ let package = Package(
             name: "VersionedCodable",
             targets: ["VersionedCodable"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    ],
     targets: [
         .target(
             name: "VersionedCodable",
